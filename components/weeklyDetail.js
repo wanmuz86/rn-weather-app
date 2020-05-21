2cf573395e8f7ff24533953248c6cef1712ed52d
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-05-21 22:21:37 +0800

    add item

--- components/weeklyDetail.js
+++ components/weeklyDetail.js
@@ -1,9 +1,32 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View,  } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 export default function WeeklyDetail({navigation}) {
 
     const weather = navigation.getParam('weather')
+    let iconUrl = "https://openweathermap.org/img/wn/"+weather["weather"][0]["icon"]+"@2x.png";
     return (
-    <View><Text>{new Date(weather.dt* 1000).toDateString()}</Text></View>
+        <View style={styles.detailStyle}>
+        <Image source={{uri:iconUrl}} style={{width:150, height:150}}/>
+        <Text>{weather["weather"][0]["main"]}</Text>
+        <Text>{weather["weather"][0]["description"]}</Text>
+        <Text>{(weather["temp"]["day"] -273.15).toFixed(2)}) C</Text>
+        <Text>Pressure: {weather["pressure"]}</Text>
+        <Text>Humidity: {weather["humidity"]}</Text>
+        <View>
+            <Text>{(new Date(weather["sunrise"]* 1000)).toTimeString()}</Text>
+        </View>
+        <View>
+            <Text>{(new Date(weather["sunset"]* 1000)).toTimeString()}</Text>
+        </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    detailStyle: {
+        flex: 1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
+})
